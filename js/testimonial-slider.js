03c6d3ab8d3f70636e0226790fbdda1c1ed7c788
Esteban Esquivel <esquivelesteban0@gmail.com>
2024-08-15 22:46:45 -0500

    adjust testimonial slider

--- js/testimonial-slider.js
+++ js/testimonial-slider.js
@@ -1,18 +1,19 @@
 // vars
 'use strict';
+
 var	testim = document.getElementById("testim"),
-		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
-    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
-    testimLeftArrow = document.getElementById("left-arrow"),
-    testimRightArrow = document.getElementById("right-arrow"),
-    testimSpeed = 4500,
-    currentSlide = 0,
-    currentActive = 0,
-    testimTimer,
-		touchStartPos,
-		touchEndPos,
-		touchPosDiff,
-		ignoreTouch = 30;
+		    testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+            testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+            testimLeftArrow = document.getElementById("left-arrow"),
+            testimRightArrow = document.getElementById("right-arrow"),
+            testimSpeed = 4500,
+            currentSlide = 0,
+            currentActive = 0,
+            testimTimer,
+		    touchStartPos,
+		    touchEndPos,
+		    touchPosDiff,
+		    ignoreTouch = 30;
 ;
 
 window.onload = function() {
@@ -45,15 +46,7 @@ window.onload = function() {
         testimTimer = setTimeout(function() {
             playSlide(currentSlide += 1);
         }, testimSpeed)
-    }
-
-    testimLeftArrow.addEventListener("click", function() {
-        playSlide(currentSlide -= 1);
-    })
-
-    testimRightArrow.addEventListener("click", function() {
-        playSlide(currentSlide += 1);
-    })    
+    } 
 
     for (var l = 0; l < testimDots.length; l++) {
         testimDots[l].addEventListener("click", function() {
